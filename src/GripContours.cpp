41d29c159ff53c6b15dc9f997abd8633c6cd3781
FRC Team Blitz #2083 <github@teamblitz.net>
2020-02-22 14:26:31 -0700

    Retroreflective tape contours

---
+++ src/GripContours.cpp
@@ -0,0 +1,110 @@
+#include "GripContours.h"
+/**
+* Initializes a GripContours.
+*/
+
+namespace grip {
+
+GripContours::GripContours() {
+}
+/**
+* Runs an iteration of the Pipeline and updates outputs.
+*
+* Sources need to be set before calling this method. 
+*
+*/
+void GripContours::process(cv::Mat source0){
+	//Step HSV_Threshold0:
+	//input
+	cv::Mat hsvThresholdInput = source0;
+	double hsvThresholdHue[] = {0.0, 140.06824766816538};
+	double hsvThresholdSaturation[] = {0.0, 27.269623881720857};
+	double hsvThresholdValue[] = {220.1438909061521, 255.0};
+	hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, this->hsvThresholdOutput);
+	//Step Find_Contours0:
+	//input
+	cv::Mat findContoursInput = hsvThresholdOutput;
+	bool findContoursExternalOnly = false;  // default Boolean
+	findContours(findContoursInput, findContoursExternalOnly, this->findContoursOutput);
+	//Step Convex_Hulls0:
+	//input
+	std::vector<std::vector<cv::Point> > convexHullsContours = findContoursOutput;
+	convexHulls(convexHullsContours, this->convexHullsOutput);
+}
+
+/**
+ * This method is a generated setter for source0.
+ * @param source the Mat to set
+ */
+void GripContours::setsource0(cv::Mat &source0){
+	source0.copyTo(this->source0);
+}
+/**
+ * This method is a generated getter for the output of a HSV_Threshold.
+ * @return Mat output from HSV_Threshold.
+ */
+cv::Mat* GripContours::gethsvThresholdOutput(){
+	return &(this->hsvThresholdOutput);
+}
+/**
+ * This method is a generated getter for the output of a Find_Contours.
+ * @return ContoursReport output from Find_Contours.
+ */
+std::vector<std::vector<cv::Point> >* GripContours::getfindContoursOutput(){
+	return &(this->findContoursOutput);
+}
+/**
+ * This method is a generated getter for the output of a Convex_Hulls.
+ * @return ContoursReport output from Convex_Hulls.
+ */
+std::vector<std::vector<cv::Point> >* GripContours::getconvexHullsOutput(){
+	return &(this->convexHullsOutput);
+}
+	/**
+	 * Segment an image based on hue, saturation, and value ranges.
+	 *
+	 * @param input The image on which to perform the HSL threshold.
+	 * @param hue The min and max hue.
+	 * @param sat The min and max saturation.
+	 * @param val The min and max value.
+	 * @param output The image in which to store the output.
+	 */
+	void GripContours::hsvThreshold(cv::Mat &input, double hue[], double sat[], double val[], cv::Mat &out) {
+		cv::cvtColor(input, out, cv::COLOR_BGR2HSV);
+		cv::inRange(out,cv::Scalar(hue[0], sat[0], val[0]), cv::Scalar(hue[1], sat[1], val[1]), out);
+	}
+
+	/**
+	 * Finds contours in an image.
+	 *
+	 * @param input The image to find contours in.
+	 * @param externalOnly if only external contours are to be found.
+	 * @param contours vector of contours to put contours in.
+	 */
+	void GripContours::findContours(cv::Mat &input, bool externalOnly, std::vector<std::vector<cv::Point> > &contours) {
+		std::vector<cv::Vec4i> hierarchy;
+		contours.clear();
+		int mode = externalOnly ? cv::RETR_EXTERNAL : cv::RETR_LIST;
+		int method = cv::CHAIN_APPROX_SIMPLE;
+		cv::findContours(input, contours, hierarchy, mode, method);
+	}
+
+	/**
+	 * Compute the convex hulls of contours.
+	 *
+	 * @param inputContours The contours on which to perform the operation.
+	 * @param outputContours The contours where the output will be stored.
+	 */
+	void GripContours::convexHulls(std::vector<std::vector<cv::Point> > &inputContours, std::vector<std::vector<cv::Point> > &outputContours) {
+		std::vector<std::vector<cv::Point> > hull (inputContours.size());
+		outputContours.clear();
+		for (size_t i = 0; i < inputContours.size(); i++ ) {
+			cv::convexHull(cv::Mat((inputContours)[i]), hull[i], false);
+		}
+		outputContours = hull;
+	}
+
+
+
+} // end grip namespace
+
